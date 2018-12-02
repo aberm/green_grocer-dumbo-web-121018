4a1c300463820f26f953fa1c4a058278420fca7d
Menachem Aber <abermendel@gmail.com>
2018-12-02 20:00:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,58 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    item.each do |name, detail|
+      if not new_hash[name]
+        new_hash[name] = detail
+        new_hash[name][:count] = 1
+      else
+        new_hash[name][:count] += 1
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
+  # binding.pry
+  i = 0
+  while i < coupons.length && coupons.length != 0
+    if cart.keys.include?(coupons[i][:item]) && cart[(coupons[i][:item])][:count] >= coupons[i][:num]
+      
+      cart[(coupons[i][:item])][:count] -= coupons[i][:num]
+      cart[(coupons[i][:item]) + " W/COUPON"] = {price: coupons[i][:cost], clearance: cart[(coupons[i][:item])][:clearance], count: 1}
+      i+=1
+    else
+      i+=1
+    end
+  end
+  # binding.pry
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  
+  cart.each do |name, details|
+    if details[:clearance] == true
+      details[:price] = (details[:price]/5)*4
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+
+  cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  total = 0
+  
+  cart.each do |name, details|
+    total += details[:price] * details[:count]
+  end
+  if total > 100
+    total = total / 10 * 9
+  end
+  total
 end
